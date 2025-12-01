85bab2fc4ba253aed38c68725a8a2920c6801245
RameshBorate <rameshborate0442@gmail.com>
2025-12-01 14:48:22 +0530

    data binding

--- src/components/data-binding/data-binding.component.ts
+++ src/components/data-binding/data-binding.component.ts
@@ -9,20 +9,49 @@ import { FormControl, FormsModule } from '@angular/forms';
 })
 export class DataBindingComponent {
 // There are 4 types : 
- country = 'India';
 
- isDiseabled = false;
+//    1. interpollation :-    one way - ts to html    {{}}            
+
+country = 'India';
+
+a = 5;
+b = 10;
+
+name = {
+  firstName: 'Ramesh',
+  lastName: 'Borate',
+  isLoggedIn : false
+};
+
+//      2. attribute / property binding :-      one way - ts to html    [] 
+
+ isDisabled = false;
+
+ isRed= true;
+
+//      3. event binding :-           one way  - html to ts    ex. (event)="function()"
+
+ onSubmit(ev:any) {
+   alert('Button clicked');
+ }
+ 
+  onSubm() {
+    alert('Form Submitted!');
+  }
+
+
+//     4. 2 way data binding :-     Import formModule and we use [(ngModel)] to bind the data
+
 
  user = {
   email : ''
  };
 
- 
- onSubmit(ev:any) {
-  alert('Button clicked');
- }
-
  saveData(form:any) {
   alert (this.user.email);
  }
+
+ username: string = '';
+
+ age: number = 25;
 }
