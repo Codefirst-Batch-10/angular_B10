a45f83f24581bbc542e630927bf2f2d6e6014a6b
Rkulkrni <rahulspk7@gmail.com>
2025-12-09 10:41:15 +0530

    Day : 5 _Services and its usecase.

--- src/components/pipe-component/pipe-component.component.ts
+++ src/components/pipe-component/pipe-component.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TruncatePipe } from "../../pipes/truncate.pipe";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-pipe-component',
@@ -17,4 +18,11 @@ export class PipeComponentComponent {
     runs : 120
   }
   description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur provident tempore ratione fugit illum eaque quia expedita ipsum sequi enim tenetur velit amet eos accusamus quibusdam odio odit, facere impedit.'
+
+  constructor(private _authService:AuthService) {}
+
+  ngOnInit() {
+    const pipe = this._authService.addition(100,200);
+    console.log(pipe);
+  }
 }
