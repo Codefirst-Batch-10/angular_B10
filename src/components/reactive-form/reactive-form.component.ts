5bcbecffd76265c9b2944539fd2613f054625503
RameshBorate <rameshborate0442@gmail.com>
2025-12-15 22:40:18 +0530

    reactive form :- datepicker and calculate age based on DOB

--- src/components/reactive-form/reactive-form.component.ts
+++ src/components/reactive-form/reactive-form.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, RequiredValidator, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators, FormControlName } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [ReactiveFormsModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatIconModule, ReactiveFormsModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css',
 })
@@ -13,26 +20,65 @@ export class ReactiveFormComponent {
   constructor(private _formBuilder: FormBuilder) {
 
     this.userForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(12),  
-        // Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])$/)
-        ]]
-    }); 
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20)]],
+      ageUser: ['', [Validators.required, Validators.max(80), Validators.min(18)]],
+      address: ['', [Validators.required, Validators.maxLength(200)]],
+      mobile: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]],
+      dob: [''],
+      age: [{ value: '', disabled: false }]
+
+    })
   }
 
+
   onSubmit() {
-    if(this.userForm.valid) {
+    if (this.userForm.valid) {
       console.log(this.userForm.value);
     }
   }
 
-  isInvalid(controlName:any) {
+  onsub() {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+
+
+    }
+  }
+
+  isInvalid(controlName: any) {
     const control = this.userForm.get(controlName);
     return control.invalid && (control.touched || control.dirty);
   }
 
-  isPasswordValid(controlName:any){
-    const passControl= this.userForm.get(controlName);
+  isPasswordValid(controlName: any) {
+    const passControl = this.userForm.get(controlName);
     return passControl.invalid && passControl.touched;
+  }
+
+  calculateAge(dob: Date): number {
+    const today = new Date();
+    const birthDate = new Date(dob);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+
+    return age;
+  }
+
+  ngOnInit() {
+    this.userForm.get('dob')?.valueChanges.subscribe((dob: Date) => {
+      if (dob) {
+        const age = this.calculateAge(dob);
+        this.userForm.get('age')?.setValue(age);
+        console.log(age);
+
+      }
+    });
   }
 }
