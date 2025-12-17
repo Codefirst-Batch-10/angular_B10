0417bb3bab3730357df131e1f76cb8ccc5c1927a
Praddip kakde <kakdepradip21@gmail.com>
2025-12-17 14:44:57 +0530

    service,databinding,controlform component created

---
+++ src/component/control-form/control-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-form',
+  imports: [],
+  templateUrl: './control-form.component.html',
+  styleUrl: './control-form.component.css'
+})
+export class ControlFormComponent {
+
+}
